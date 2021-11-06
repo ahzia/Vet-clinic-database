76cc8a25e520ee1e5fd5d24b4c80cdd724a358ca
Ahmad Zia <ah.ziayosfi@gmail.com>
2021-11-07 01:13:47 +0430

    create table for vets

--- schema.sql
+++ schema.sql
@@ -36,4 +36,11 @@ ALTER TABLE animals ADD COLUMN species_id INT,
 ALTER TABLE animals ADD COLUMN owner_id INT,
   ADD CONSTRAINT fk_owners
   FOREIGN KEY (owner_id)
-  REFERENCES owners (id); 
+  REFERENCES owners (id);
+
+CREATE TABLE vets(
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  age INT,
+  date_of_graduation date
+);
